4c78964270a971bc32555160fabc54440ee526c8
Cory Carter <cory.and.finn@gmail.com>
2021-04-04 13:24:18 +0100

    Remembers where you started

--- index.js
+++ index.js
@@ -30,6 +30,30 @@ const oldway = () =>{
     }
     
 }
+const skip = option =>{
+    if (!option.with && !option.without){
+        return false;
+    }
+    const name = option.with == null ? option.without: option.with;
+    const holding = localStorage.getItem(name);
+    return (holding == null && option.with) || (holding != null && option.without);
+}
+
+const process = option  =>{
+    console.log(option);
+    if (skip(option)){
+        return;
+    }
+    const li = document.createElement('li');
+    li.innerHTML = option.desc;
+    const json = option.link;
+    const action = (event)=>{
+        go(json);
+        document.removeEventListener("click", li);
+    }
+    li.addEventListener("click", action);
+    return li;
+}
 
 const build = (data) =>{
     const titleElement = document.getElementById('title');
@@ -43,21 +67,20 @@ const build = (data) =>{
     });
     const optionsElement = document.getElementById("options");
     removeChilds(optionsElement);
-    data.options.forEach(option =>{
-        console.log(option);
-        const li = document.createElement('li');
-        li.innerHTML = option.desc;
-        const json = option.link;
-        const action = (event)=>{
-            go(json);
-            document.removeEventListener("click", li);
+    data.options.forEach(option => {
+        const li = process(option);
+        if (li){
+            optionsElement.appendChild(li);
         }
-        li.addEventListener("click", action);
-        optionsElement.appendChild(li);
     });
 }
 
-const go = async (data = 'index.json') => {
+const go = async (place) => {
+    if (place == null){
+        place = localStorage.getItem("current");
+    }
+    const data = place == null? 'index.json': place;
+    localStorage.setItem("current", data);
     console.log("We are now going load ",data);
     const dataString = await load(data);
     console.log(dataString);
